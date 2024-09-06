4a3b895802516511c24fe372a2402f08e6f25f14
Rahul BK <rlbishwokarma@gmail.com>
2024-09-06 09:36:08 +0545

    Feature: DB connection setup and user model setup

--- src/config/db-config.ts
+++ src/config/db-config.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URL || "");
-    console.log("✔️ Mongodb connected");
+    console.log("✔  Mongodb connected");
   } catch (error) {
     console.log(`❌ Error connecting to MongoDB`, error);
   }
